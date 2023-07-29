ae904ef5259aa61ff3433f52b2c4de120a7c3a44
Hans <htja0002@student.monash.edu>
2023-07-30 07:13:49 +1000

    boyer_moore og implementation fixed

--- q1/oppositeway_boyermoore.py
+++ q1/oppositeway_boyermoore.py
@@ -1,12 +1,174 @@
 import sys
 
-#boyermoore algorithm in the opposite direction
-#scanning from left to right 
-#iteration starts from the most right instead of the most left
-def opposite_boyermoore(text, pat):
-    output = []
-    return output
+#constants
+STARTING_ASCII = 97
+NUM_ALPHABET = 26
 
+def q2(txt, pat):
+    
+    return oppositeway_boyermoore(txt, pat)
+
+#allows a char "." which matches with any string in the 26 alphabet 
+def oppositeway_boyermoore(txt, pat):
+    m = len(pat)
+    n = len(txt)
+    i = m - 1
+    pat_i = m - 1
+    z = [0 for _ in range(m)]
+    R = None
+    z_suffix = None
+    matched_prefix = None
+    good_suffix = None
+    to_shift = 0
+    occurence = []
+
+
+    #compute z_value 
+    z = z_algo(pat) #(DONE)
+
+    #compute Rk(x)
+    R = compute_R(pat)  #(DONE)
+
+    #compute z_suffix 
+    z_suffix = compute_z_suffix(pat) #(DONE)
+
+    #compute good_suffix
+    good_suffix = compute_good_suffix(pat)   #(DONE)
+
+    #compute matched_prefix
+    matched_prefix = compute_matched_prefix(pat) #(DONE)
+
+
+    #main iteration of boyer_moore
+    while i < n:
+
+        text_i = i
+        k = pat_i
+        j = text_i - (m - 1)    #starting substring region in txt
+        while pat_i > -1 and txt[text_i] == pat[pat_i]:
+            text_i -= 1
+            pat_i -= 1
+            k = pat_i
+        
+        
+        x = txt[j + k]
+        b_shift = bad_char_shift(x, k, R)
+        matched, g_shift = good_suffix_shift(good_suffix, matched_prefix,k, m)
+
+        to_shift = max(b_shift, g_shift)
+
+        if matched:
+            occurence.append(j)
+
+        i += to_shift - 1
+        pat_i = m - 1
+
+    return occurence
+
+#function to compute Rk(x) value
+#need to modify to be able to use wildcard
+def compute_R(pat):
+    R = [[-1 for _ in range(NUM_ALPHABET)] for _ in range(len(pat))]
+
+    for i in range(1, len(pat)):
+        
+        # if iteration is at index 1
+        if i == 1:
+
+            # # store index of the rightmost occurence of a char into R-list
+            # occurence of char i - 1 of current iteration
+            R[i][ord(pat[i - 1]) - STARTING_ASCII] = i - 1
+        # any other iteration
+        else:
+            # Copy the value of 
+            for j in range(NUM_ALPHABET):
+
+                #Copy when value index is available
+                if R[i-1][j] > -1:
+                    R[i][j] = R[i-1][j]
+
+            # store index of the rightmost occurence of a char into R-list
+            # occurence of char i - 1 of current iteration
+            R[i][ord(pat[i-1]) - STARTING_ASCII] = i - 1
+
+
+    return R
+
+#function to compute matched_prefix value
+#solution from q7 week 2 tute
+def compute_matched_prefix(pat):
+    m_prefix = [0 for _ in range(len(pat) + 1)]
+
+    z = z_algo(pat)
+    m_prefix[0] = len(pat)
+    
+    for i in range(len(pat) - 1, 0, -1):
+        if z[i]+i == len(pat):
+            m_prefix[i] = z[i]  #found a new largest suffix that matches its prefix
+        else:
+            m_prefix[i] = m_prefix[i+1] #otherwise, previously found value is copied
+    
+    return m_prefix
+
+#function to compute z_suffix
+def compute_z_suffix(pat):
+    inv_pat = ""
+    inv_z = None
+    z_suffix = []
+    for i in range(len(pat) - 1, -1, -1):
+        inv_pat += pat[i]
+
+    inv_z = z_algo(inv_pat)
+
+    for i in range(len(inv_z) - 1, -1, -1):
+        z_suffix.append(inv_z[i])
+    
+    return z_suffix
+
+#function to compute good_suffix
+def compute_good_suffix(pat):
+    z_suffix = compute_z_suffix(pat)
+    m = len(pat)
+    g_sfx = [0 for _ in range(m+1)]
+
+    for p in range(m-1):
+        j = m - z_suffix[p]
+        g_sfx[j] = p
+
+    return g_sfx
+
+
+#function to compute shfit by bad_char rule
+def bad_char_shift(x, k, R):
+
+    #only if there is a missmatch
+    if k is not None:
+        if R[k][ord(x)-STARTING_ASCII] == -1:
+            return 1
+        else:
+            return k - R[k][ord(x)-STARTING_ASCII]    
+
+
+
+#function to compute shift by good_suffix rule
+def good_suffix_shift(g_sffx, m_pref, k, m):
+    
+    #case 1a
+    if g_sffx[k+1] > 0:
+        return False, m - g_sffx[k+1]
+
+    #case 1b
+    elif g_sffx[k+1] == -1:
+        return False, m - m_pref[k+1]
+
+    #case 2 (when the whole substring matches with the pattern)
+    elif k < 0:
+        return True, m - m_pref[1]
+    
+    #return 0 shift if no cases match
+    else:
+        return False, 0
+        
 #z_algorithm main iteration
 #need to modify to be able to use wild card
 def z_algo(s):
@@ -100,36 +262,39 @@ def case_2b(s,k,r,l):
 
     return total_count, start_z_box, end_z_box
 
-#open and read a file of specified filename
-def read_file(filename):
-    f = open(filename, 'r')
-    f_name = f.readline()
-    f.close()
+#function to read input from given text_file_name
+def read_input(fileName):
+    f = open(fileName, 'r')
+    content = f.readline()
+    f.close
 
-    return f_name
+    return content
 
-#create a file of specified filename
-#write the output of the computation into a file
-def write_file(output):
-    f = open("output_kmp.txt", 'w')
-    f.write(output[0])
-
+#function to write into a txt file of given text_file_name
+#output = .txt file
+def write_output(output):
+    f = open('output_q2.txt','w')
+    f.write(str(output[0]))
     for i in range(1, len(output)):
         f.write("\n")
-        f.write(output[i])
-
+        f.write(str(output[i]))
     f.close()
 
 if __name__ == '__main__':
-    text_file_name = sys.argv[1]
-    pat_file_name = sys.argv[2]
 
-    text = read_file(text_file_name)
-    pat = read_file(pat_file_name)
+    """ textFileName, patFileName = sys.argv[1], sys.argv[2]
 
-    print(text)
-    print(pat)
+    txt, pat = read_input(textFileName), read_input(patFileName) """
 
-    """ output = opposite_boyermoore(text, pat)
+    txt = "abcdabcdabcd"
+    pat = "abc"
+    output = q2(txt, pat)
 
-    write_file(output) """
+    print(output)
+
+    """ write_output(output) """
+
+
+
+
+
